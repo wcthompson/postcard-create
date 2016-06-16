829fc025eb51c43b592efaef8862de37a14d2a9c
Will Thompson <will@lob.com>
2016-06-16 15:27:12 -0700

    feat(services): created a postcard create service with first unit test

--- app/services/index.js
+++ app/services/index.js
@@ -3,14 +3,16 @@
 var Angular = require('angular');
 var Cookies = require('angular-cookies');
 
-var API     = require('./api');
-var Auth    = require('./auth');
-var Session = require('./session');
+var API      = require('./api');
+var Auth     = require('./auth');
+var Postcard = require('./postcard');
+var Session  = require('./session');
 
 module.exports = Angular.module('postcard-create.services', [
   Cookies
 ])
 .factory('API', API)
 .factory('Auth', Auth)
+.factory('Postcard', Postcard)
 .factory('Session', Session)
 .name;
